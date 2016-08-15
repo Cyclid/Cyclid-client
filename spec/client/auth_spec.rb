cc8bfba6217b86ec071fdd9880d3fd86ccf6d83f
Kristian Van Der Vliet <vanders@liqwyd.com>
2016-08-15 19:06:51 +0100

    Add tests for token_get

---
+++ spec/client/auth_spec.rb
@@ -0,0 +1,24 @@
+require 'client_helper'
+
+describe Cyclid::Client::Auth do
+  context 'retrieving an authentication token' do
+    before :all do
+      @client = Cyclid::Client::Tilapia.new(path: ENV['TEST_CONFIG'])
+    end
+
+    it 'retrieves an API token' do
+      stub_request(:post, "http://localhost:9999/token/test")
+        .with(body: '"sekrit"',
+              headers: { 'Accept' => '*/*',
+                         'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                         'Authorization' => /\AHMAC admin:.*\z/,
+                         'Host' => 'localhost:9999',
+                         'User-Agent' => 'Ruby',
+                         'Date' => /.*/,
+                         'X-Hmac-Nonce' => /.*/ })
+        .to_return(:status => 200, :body => '{}', :headers => {})
+
+      expect{ token = @client.token_get('test', 'sekrit') }.to_not raise_error
+    end
+  end
+end
